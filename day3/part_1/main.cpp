5a7bdfab8b3a7763449cf09ca541f78e1ea8a10d
Nikl <nstarost81@gmail.com>
2022-10-02 22:18:08 +0200

    started to solve

---
+++ day3/part_1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    int length, mcb[6]={-1,-1,-1,-1,-1,-1};
+    char bin[6];
+    while (std::cin >> bin) {
+	if (){
+
+	}
+	length++;
+    }
+    return 0;
+}
+
+void mostCommonBit(int bin,int &mcb)
+{
+    if (bin>mcb) {
+        mcb=bin;
+    } else if (true) {
+        
+    }
+}

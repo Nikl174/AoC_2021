d23b7e803fda5e6b488a78e59eb7596d45904e9a
Nikl <nstarost81@gmail.com>
2022-10-03 23:32:30 +0200

    solved day3 part 1

--- day3/part_1/main.cpp
+++ day3/part_1/main.cpp
@@ -1,23 +1,43 @@
+#include <bitset>
 #include <iostream>
 
-int main(int argc, char *argv[])
-{
-    int length, mcb[6]={-1,-1,-1,-1,-1,-1};
-    char bin[6];
-    while (std::cin >> bin) {
-	if (){
+#define SIZE 12
 
-	}
-	length++;
+void mostCommonBit(char bin[SIZE + 1], int mcb[SIZE]) {
+  for (int i = 0; i < SIZE; i++) {
+    switch (bin[i]) {
+    case '1':
+      mcb[i] += 1;
+      break;
+    case '0':
+      mcb[i] -= 1;
+      break;
+    default:
+      std::cout << "FEHLER!" << std::endl;
+      return;
     }
-    return 0;
+  }
 }
 
-void mostCommonBit(int bin,int &mcb)
-{
-    if (bin>mcb) {
-        mcb=bin;
-    } else if (true) {
-        
-    }
+int main(int argc, char *argv[]) {
+  int length = 0, mcb[SIZE] = {0};
+  char bin[SIZE + 1];
+  while (std::cin >> bin) {
+    mostCommonBit(bin, mcb);
+    length++;
+  }
+  unsigned int i, gamma = 0, epsilon=0;
+  for (i = 0; i < SIZE; ++i) {
+    bool binI = !( mcb[i] & (0x80000000) );
+    std::cout << "binI="<<binI<<"; mcb="<<mcb[i] << std::endl;
+    gamma += (binI << (SIZE - i-1));
+    epsilon +=(!binI << (SIZE - i-1));
+  }
+  //wrong because of th first bits -> 32bit integer value..
+  //unsigned int epsilon = ~gamma;
+  std::cout << gamma << "; " << std::bitset<12>(gamma) << std::endl;
+  std::cout << epsilon << "; " << std::bitset<12>(epsilon)<< std::endl;
+
+  std::cout << "Solution gamma*epsilon="<<gamma*epsilon << std::endl;
+  return 0;
 }
